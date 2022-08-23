f6fe930a9ce8ad1c34a5439c3c16744154fb9f42
Appoline Cogan <appolinecogan@gmail.com>
2022-08-23 11:33:42 +0700

    created db and tables for role, employee and dpt

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE DATABASE company_db;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+    id INT PRIMARY KEY,
+    title VARCHAR(30), 
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30), 
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY role_id
+    REFERENCES roles(id)
+);
