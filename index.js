814502638625c2f6a36b9e225a84940e3cf63505
Appoline Cogan <appolinecogan@gmail.com>
2022-08-27 10:50:50 +0700

    created init function with initial inquirer prompts and created switch case for each possible answer

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // WHEN I start the application
 // THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 // WHEN I choose to view all departments
@@ -14,10 +13,68 @@
 // WHEN I choose to add an employee
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 // WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+const mysql = require("mysql2");
+
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "company_db",
 });
+
+// function init with all options to choose from
+init = () => {
+  return inquirer
+    .prompt({
+      type: "list",
+      message: "What would you like to do?",
+      name: "viewAll",
+      choices: [
+        "View all departments",
+        "View all Roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update and employee role",
+      ],
+    })
+    .then(
+      (makeChoice = (response) => {
+        switch (response.viewAll) {
+          case "View all departments":
+            return viewAllDpts();
+
+          case "View all Roles":
+            return viewAllRoles();
+
+          case "View all employees":
+            return viewAllEmployees();
+
+          case "Add a department":
+            return addDpt();
+
+          case "Add a role":
+            return addRole();
+
+          case "Add an employee":
+            return addEmployee();
+
+          default:
+            "Update and employee role";
+            return updateEmployee();
+        }
+      })
+    );
+};
+
+function viewAllDpts() {}
+function viewAllRoles() {}
+function viewAllEmployees() {}
+function addDpt() {}
+function addEmployee() {}
+function addRole() {}
+function updateEmployee() {}
