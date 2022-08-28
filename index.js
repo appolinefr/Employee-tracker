5ccbe885f16cd9749e8241ae5540784d9f8c7ef7
Appoline Cogan <appolinecogan@gmail.com>
2022-08-28 14:34:25 +0700

    added comments through index.js file and refactored code

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 const mysql = require("mysql2");
@@ -107,7 +91,7 @@ function viewAllEmployees() {
     init();
   });
 }
-
+// function to add a dept with prepared statement for adding info into dept table
 function addDpt() {
   return inquirer
     .prompt([
@@ -126,12 +110,13 @@ function addDpt() {
         if (err) {
           console.log(`Error in adding department`);
         }
-        console.log(`Department has been added`);
+        console.log(`${department.name} has been added to the database`);
         init();
       });
     });
 }
-
+// function to add an employee with prepared statement for adding info into employee table
+ // I need to be able to extract all roles/manager id from database and display them in list in prompt
 function addEmployee() {
   return inquirer
     .prompt([
@@ -183,12 +168,15 @@ function addEmployee() {
         if (err) {
           console.log(`Error in adding Employee`);
         }
-        console.log(`Employee has been added`);
+        console.log(
+          `${employee.firstName} ${employee.lastName} has been added to the database`
+        );
         init();
       });
     });
 }
-
+// function to add a role with prepared statement for adding info into role table
+ // I need to be able to extract all role from database and display them in list in prompt
 function addRole() {
   return inquirer
     .prompt([
@@ -206,7 +194,7 @@ function addRole() {
         type: "list",
         message: "Which department does the role belong to?",
         name: "roleDpt",
-        choices: [],
+        choices: [], // I need to be able to extract all role from database and display them in list
       },
     ])
     .then((response) => {
@@ -222,10 +210,10 @@ function addRole() {
         if (err) {
           console.log(`Error in adding Role`);
         }
-        console.log(`Role has been added`);
+        console.log(`${role.name} has been added to the database`);
         init();
       });
     });
 }
-
+//need to work on this last function
 function updateEmployee() {}
