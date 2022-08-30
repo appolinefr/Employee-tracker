5b22fdabff30c336c107c6922f6ab6decd5e68de
Appoline Cogan <appolinecogan@gmail.com>
2022-08-30 22:33:13 +0700

    cleaned up code and added final comments

--- index.js
+++ index.js
@@ -74,6 +74,9 @@ init = () => {
 };
 
 init();
+
+//functions to view dpts, roles and employees from the database
+
 // function to view all departments with prepared statement for querying the relevant table
 function viewAllDpts() {
   connection.query(
@@ -117,201 +120,6 @@ function viewAllEmployees() {
       init();
     }
   );
-}
-// function to add a dept with prepared statement for adding info into dept table
-async function addDpt() {
-  const response = await inquirer.prompt([
-    {
-      type: "input",
-      message: "What is the name of the department?",
-      name: "departmentName",
-    },
-  ]);
-
-  const department = response.departmentName;
-  const sql = `INSERT INTO department (department_name)
-  VALUES (?)`;
-
-  connection.query(sql, department, (err) => {
-    if (err) {
-      console.log(`Error in adding department`);
-    }
-    console.log(`\n ${department} has been added to the database \n `);
-    init();
-  });
-}
-
-const getRoles = () => {
-  return new Promise((resolve, reject) => {
-    const sql = `SELECT roles.title, roles.id FROM roles`;
-    connection.query(sql, (err, results) => {
-      if (err) reject(err);
-      resolve(results);
-    });
-  });
-};
-
-const getManagers = () => {
-  return new Promise((resolve, reject) => {
-    const sql = `SELECT employee.first_name, employee.last_name, employee.id, employee.manager_id FROM employee WHERE employee.manager_id IS NULL`;
-    connection.query(sql, (err, results) => {
-      if (err) reject(err);
-      resolve(results);
-    });
-  });
-};
-
-// function to add an employee with prepared statement for adding info into employee table
-async function addEmployee() {
-  const roles = await getRoles();
-  const managers = await getManagers();
-  const response = await inquirer.prompt([
-    {
-      type: "input",
-      message: "What is the employee's first name?",
-      name: "employeeFirstName",
-    },
-    {
-      type: "input",
-      message: "What is the employee's last name?",
-      name: "employeeLastName",
-    },
-    {
-      type: "list",
-      message: "What is the role of the employee?",
-      name: "employeeRole",
-      choices: roles.map(({ title, id }) => ({
-        name: title,
-        value: id,
-      })),
-    },
-    {
-      type: "list",
-      message: "Who is the manager of the employee?",
-      name: "employeeManager",
-      choices: managers.map(({ id, first_name, last_name }) => ({
-        name: first_name + " " + last_name,
-        value: id,
-      })),
-    },
-  ]);
-
-  const params = [
-    response.employeeFirstName,
-    response.employeeLastName,
-    response.employeeRole,
-    response.employeeManager,
-  ];
-
-  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
-  VALUES (?,?,?,?)`;
-
-  connection.query(sql, params, (err) => {
-    if (err) {
-      console.log(`Error in adding Employee`);
-    }
-    console.log(
-      `\n ${response.employeeFirstName} ${response.employeeLastName} has been added to the database \n `
-    );
-    init();
-  });
-}
-
-const getDpts = () => {
-  return new Promise((resolve, reject) => {
-    const sql = `SELECT department.id, department.department_name FROM department`;
-    connection.query(sql, (err, results) => {
-      if (err) reject(err);
-      resolve(results);
-    });
-  });
-};
-
-// function to add a role with prepared statement for adding info into role table
-async function addRole() {
-  const departments = await getDpts();
-  const response = await inquirer.prompt([
-    {
-      type: "input",
-      message: "What is the name of the role?",
-      name: "roleName",
-    },
-    {
-      type: "input",
-      message: "What is the salary of the role?",
-      name: "roleSalary",
-    },
-    {
-      type: "list",
-      name: "roleDpt",
-      message: "What department is this role in?",
-      choices: departments.map(({ department_name, id }) => ({
-        name: department_name,
-        value: id,
-      })),
-    },
-  ]);
-
-  const params = [response.roleName, response.roleSalary, response.roleDpt];
-
-  const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`;
-
-  connection.query(sql, params, (err) => {
-    if (err) {
-      console.log(`Error in adding role`);
-    }
-    console.log(`\n Role has been updated in the database \n `);
-    init();
-  });
-
-  init();
-}
-
-
-const getEmployees = () => {
-  return new Promise((resolve, reject) => {
-    const sql = `SELECT employee.id, employee.first_name, employee.last_name FROM employee`;
-    connection.query(sql, (err, results) => {
-      if (err) reject(err);
-      resolve(results);
-    });
-  });
-};
-
-//gets the employee's name and get the role then assign new role
-async function updateEmployee() {
-  const employees = await getEmployees();
-  const roles = await getRoles();
-  const response = await inquirer.prompt([
-    {
-      type: "list",
-      message: "Which employee would you like to update?",
-      name: "employeeName",
-      choices: employees.map(({ id, first_name, last_name }) => ({
-        name: first_name + " " + last_name,
-        value: id,
-      })),
-    },
-    {
-      type: "list",
-      message: "What is the new role for this employee?",
-      name: "updateRole",
-      choices: roles.map(({ title, id }) => ({
-        name: title,
-        value: id,
-      })),
-    },
-  ]);
-
-  const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
-
-  connection.query(sql, [response.updateRole, response.employeeName], (err) => {
-    if (err) {
-      console.log(`Error in updating Employee`);
-    }
-    console.log(`\n Role has been updated in the database \n `);
-    init();
-  });
 }
 
 // View employees by department.
@@ -368,6 +176,48 @@ async function viewEmployeeManager() {
   });
 }
 
+//functions to add information to database
+
+const getRoles = () => {
+  return new Promise((resolve, reject) => {
+    const sql = `SELECT roles.title, roles.id FROM roles`;
+    connection.query(sql, (err, results) => {
+      if (err) reject(err);
+      resolve(results);
+    });
+  });
+};
+
+const getManagers = () => {
+  return new Promise((resolve, reject) => {
+    const sql = `SELECT employee.first_name, employee.last_name, employee.id, employee.manager_id FROM employee WHERE employee.manager_id IS NULL`;
+    connection.query(sql, (err, results) => {
+      if (err) reject(err);
+      resolve(results);
+    });
+  });
+};
+
+const getDpts = () => {
+  return new Promise((resolve, reject) => {
+    const sql = `SELECT department.id, department.department_name FROM department`;
+    connection.query(sql, (err, results) => {
+      if (err) reject(err);
+      resolve(results);
+    });
+  });
+};
+
+const getEmployees = () => {
+  return new Promise((resolve, reject) => {
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name FROM employee`;
+    connection.query(sql, (err, results) => {
+      if (err) reject(err);
+      resolve(results);
+    });
+  });
+};
+
 //Getting the employees excluding managers
 const getEmpOnly = () => {
   return new Promise((resolve, reject) => {
@@ -378,6 +228,162 @@ const getEmpOnly = () => {
     });
   });
 };
+
+// function to add a dept
+async function addDpt() {
+  const response = await inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the name of the department?",
+      name: "departmentName",
+    },
+  ]);
+
+  const department = response.departmentName;
+  const sql = `INSERT INTO department (department_name)
+  VALUES (?)`;
+
+  connection.query(sql, department, (err) => {
+    if (err) {
+      console.log(`Error in adding department`);
+    }
+    console.log(`\n ${department} has been added to the database \n `);
+    init();
+  });
+}
+
+// function to add an employee
+async function addEmployee() {
+  const roles = await getRoles();
+  const managers = await getManagers();
+  const response = await inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the employee's first name?",
+      name: "employeeFirstName",
+    },
+    {
+      type: "input",
+      message: "What is the employee's last name?",
+      name: "employeeLastName",
+    },
+    {
+      type: "list",
+      message: "What is the role of the employee?",
+      name: "employeeRole",
+      choices: roles.map(({ title, id }) => ({
+        name: title,
+        value: id,
+      })),
+    },
+    {
+      type: "list",
+      message: "Who is the manager of the employee?",
+      name: "employeeManager",
+      choices: managers.map(({ id, first_name, last_name }) => ({
+        name: first_name + " " + last_name,
+        value: id,
+      })),
+    },
+  ]);
+
+  const params = [
+    response.employeeFirstName,
+    response.employeeLastName,
+    response.employeeRole,
+    response.employeeManager,
+  ];
+
+  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+  VALUES (?,?,?,?)`;
+
+  connection.query(sql, params, (err) => {
+    if (err) {
+      console.log(`Error in adding Employee`);
+    }
+    console.log(
+      `\n ${response.employeeFirstName} ${response.employeeLastName} has been added to the database \n `
+    );
+    init();
+  });
+}
+
+// function to add a role
+async function addRole() {
+  const departments = await getDpts();
+  const response = await inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the name of the role?",
+      name: "roleName",
+    },
+    {
+      type: "input",
+      message: "What is the salary of the role?",
+      name: "roleSalary",
+    },
+    {
+      type: "list",
+      name: "roleDpt",
+      message: "What department is this role in?",
+      choices: departments.map(({ department_name, id }) => ({
+        name: department_name,
+        value: id,
+      })),
+    },
+  ]);
+
+  const params = [response.roleName, response.roleSalary, response.roleDpt];
+
+  const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`;
+
+  connection.query(sql, params, (err) => {
+    if (err) {
+      console.log(`Error in adding role`);
+    }
+    console.log(`\n Role has been updated in the database \n `);
+    init();
+  });
+
+  init();
+}
+
+//gets the employee's name and get the role then assign new role
+async function updateEmployee() {
+  const employees = await getEmployees();
+  const roles = await getRoles();
+  const response = await inquirer.prompt([
+    {
+      type: "list",
+      message: "Which employee would you like to update?",
+      name: "employeeName",
+      choices: employees.map(({ id, first_name, last_name }) => ({
+        name: first_name + " " + last_name,
+        value: id,
+      })),
+    },
+    {
+      type: "list",
+      message: "What is the new role for this employee?",
+      name: "updateRole",
+      choices: roles.map(({ title, id }) => ({
+        name: title,
+        value: id,
+      })),
+    },
+  ]);
+
+  const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+
+  connection.query(sql, [response.updateRole, response.employeeName], (err) => {
+    if (err) {
+      console.log(`Error in updating Employee`);
+    }
+    console.log(`\n Role has been updated in the database \n `);
+    init();
+  });
+}
+
 // Update employee managers.
 async function updateManager() {
   const employees = await getEmpOnly();
@@ -417,6 +423,8 @@ async function updateManager() {
     }
   );
 }
+
+//Functions to delete from database
 
 //init function to delete from database
 function deleteFromDb() {
@@ -462,7 +470,6 @@ async function deleteDpt() {
   ]);
 
   const sql = `DELETE FROM department WHERE id = ?`;
-  console.log(response.dptDelete);
 
   connection.query(sql, response.dptDelete, (err) => {
     if (err) {
@@ -492,15 +499,12 @@ async function deleteRole() {
   ]);
 
   const sql = `DELETE FROM roles WHERE id = ?`;
-  console.log(response.roleDelete);
 
   connection.query(sql, response.roleDelete, (err) => {
     if (err) {
       console.log(`Error in deleting role`);
     } else {
-      console.log(
-        `\n ${response.roleDelete} has been deleted from the database \n `
-      );
+      console.log(`\n Role has been deleted from the database \n `);
     }
     init();
   });
@@ -522,15 +526,12 @@ async function deleteEmp() {
   ]);
 
   const sql = `DELETE FROM employee WHERE id = ?`;
-  console.log(response.employeeDelete);
 
   connection.query(sql, response.employeeDelete, (err) => {
     if (err) {
       console.log(`Error in deleting employee`);
     } else {
-      console.log(
-        `\n ${response.employeeDelete} has been deleted from the database \n `
-      );
+      console.log(`\n Employee has been deleted from the database \n `);
     }
     init();
   });
